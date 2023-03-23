753857a28d7daeec0a70bb77fc560760fe75d714
Mich2504 <moralesmichelle197@gmail.com>
2023-03-23 02:39:52 -0700

    Finish Class 1

---
+++ POOConcepts/POOConcepts/Date.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOConcepts
+{
+    public class Date
+    {
+        //Declaracion de atributos sin anderline private int year;
+        private int _year;// con anderline
+        private int _month;
+        private int _day;
+
+        //Crear constructor por un snifer ctor tab tab
+        //Parametros a pasar anio, mes y dia
+        public Date(int year, int month, int day)
+        {
+           _year = year;//opcion 1 this.year = year;
+            _month=CheckMonth(month);//creacion de metodo
+            _day = CheckDay(year,month,day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            //Validacion de anio bisiesto
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+            //crear un arreglo que devuelve los dias de cada mes
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+
+            throw new DayException("Invalid day");
+        }
+        private bool IsLeapYear(int year)
+        {
+            //opcion 2 simplificada
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+           //Opcion 1
+            //if (year % 4 == 0)
+            //{
+            //    if (year % 100 == 0)
+            //    {
+            //        if (year % 400 == 0)
+            //        {
+            //            return true;
+            //        }
+            //        else
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        return true;
+            //    }
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+        }
+
+
+        private int CheckMonth(int month)
+        {
+            if(month>=1 && month<=12) {//valida si el mes esta entre el rango de 1 y 12
+                return month;
+            }
+            throw new MonthException("Invalid month");//Se crea una excepcion en caso de que el rango diferente
+        }
+
+        //sobre escritura de toString
+
+        override 
+            public string ToString()
+        {
+           // return _year+ "/"+ _month+"/"+ _day;//opcion 1 consume recursos
+            return $"{_year}/{_month:00}/{_day:00}";  //opcion 2 interpolacion
+        }
+
+    }
+}
---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("=============");
+
+
+//presiona ctl k s  y escoge try 
+try
+{
+	Date date1 = new Date(2023, 3, 21);//opcion 1 para imprimir un objeto
+	Console.WriteLine(date1);
+    Console.WriteLine(new Date(2022, 2, 29));
+    Console.WriteLine(new Date(2022, 12, 7));//opcion 2 simplificada
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 18, 45));
+    Console.WriteLine(new Date(1985, 11, 45));
+}
+catch (Exception error)//excepcion que muestra un mensaje 
+{
+	Console.WriteLine(error.Message);
+	
+}
